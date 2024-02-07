e6211aba0f0d4938a496f63dcc0040f73ca4d6b4
GauthierWebDev <gauthier@gauthierdaniels.fr>
2024-02-07 13:23:34 +0100

    refactor: Remove unused column decorator

--- src/decorators/column.ts
+++
@@ -1,8 +0,0 @@
-const column = (columnType: string) => {
-  return (target: any, key: string) => {
-    if (!target.constructor._columns) target.constructor._columns = {};
-    target.constructor._columns[key] = columnType;
-  };
-};
-
-export default column;
--- src/decorators/index.ts
+++ src/decorators/index.ts
@@ -1,5 +1,4 @@
 export { default as autoInstantiate } from "./autoInstantiate";
 export { default as middleware } from "./middleware";
-export { default as column } from "./column";
 export { default as method } from "./method";
 export { default as route } from "./route";
